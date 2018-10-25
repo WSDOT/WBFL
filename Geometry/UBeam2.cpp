d81394c0121fa654ac8bdc85aaa9b72d406f6ced
Richard Brice <BriceR@wsdot.wa.gov>
2018-10-25 12:02:30 -0700

    3.6.0.2

--- Geometry/UBeam2.cpp
+++ Geometry/UBeam2.cpp
@@ -648,7 +648,17 @@ STDMETHODIMP CUBeam2::get_TopFlangeWidth(Float64* width)
 {
    CHECK_RETVAL(width);
 
-   (*width) = m_W6;
+   if (m_bUseOutlineOnly == VARIANT_TRUE)
+   {
+      // if the shape is just the outline of the U-beam,
+      // then the top flange is half the width of the beam
+      (*width) = m_W2/2;
+   }
+   else
+   {
+      // otherwise it is the normal input value
+      (*width) = m_W6;
+   }
 
    return S_OK;
 }
