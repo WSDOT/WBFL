160572e7959f4c94ab262907394e2ba4801faed7
Joshua Hudson <hudsonj@wsdot.wa.gov>
2018-10-25 12:21:23 -0700

    3.7.0.1

--- Fem2d/SymBandedMatrix.h
+++ Fem2d/SymBandedMatrix.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////
-// Copyright © 1999-2012  Washington State Department of Transportation
+// Copyright © 1999-2013  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This library was developed as part of the Alternate Route Project
