c0da5e0d2171df03a515af639d5c1ea2a564fd97
Rick Brice <bricer@wsdot.wa.gov>
2018-11-10 06:00:13 -0800

    Fixed a build dependency

--- InstallWBFL.bat
+++ InstallWBFL.bat
@@ -1,6 +1,8 @@
 REM use this script to install WBFL into %3
-xcopy /y/d %WBFLDIR%\RegFreeCOM\%1\%2\WBFL*.lib %3\%1\%2
-xcopy /y/d %WBFLDIR%\RegFreeCOM\%1\%2\WBFL*.dll %3\%1\%2
-xcopy /y/d %WBFLDIR%\RegFreeCOM\%1\%2\MakeDocMap.exe %3\%1\%2
+xcopy /y/d %WBFLDIR%\RegFreeCOM\%1\%2\WBFL*.lib %3\%1\%2\
+xcopy /y/d %WBFLDIR%\RegFreeCOM\%1\%2\WBFL*.dll %3\%1\%2\
+xcopy /y/d %WBFLDIR%\RegFreeCOM\%1\%2\MakeDocMap.exe %3\%1\%2\
+xcopy /y/d %WBFLDIR%\deps\xerces-c_3_1.dll %3\%1\Release\
+xcopy /y/d %WBFLDIR%\deps\xerces-c_3_1D.dll %3\%1\Debug\
 
 
