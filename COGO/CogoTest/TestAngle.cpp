06f2ae57b411de562ff110819f684b682028d99e
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2021-05-12 08:32:34 -0700

    Updates copyright to 2021

--- COGO/CogoTest/TestAngle.cpp
+++ COGO/CogoTest/TestAngle.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////
 // COGOTest - Test Driver for Coordinate Geometry Library
-// Copyright © 1999-2020  Washington State Department of Transportation
+// Copyright © 1999-2021  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This library is a part of the Washington Bridge Foundation Libraries
--- Loading/LoadingTest/TestTemperatureLoad.h
+++ Loading/LoadingTest/TestTemperatureLoad.h
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////
 // Loading Test - Test driver for Loading library
-// Copyright © 1999-2020  Washington State Department of Transportation
+// Copyright © 1999-2021  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This library is a part of the Washington Bridge Foundation Libraries
