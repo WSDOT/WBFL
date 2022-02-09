23428b90350d3b8e1929ce5fda21740f1f8bdc45
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2022-02-09 15:32:19 -0800

    Updated version numbers for 7.0 Beta 4

--- UpdateBinFiles.bat
+++ UpdateBinFiles.bat
@@ -7,12 +7,9 @@ set BINTARGET=bin
 
 xcopy /y /d  "RegFreeCOM\x64\Release\WBFL*.dll"	"%BINTARGET%\Libraries\x64\"
 xcopy /y /d  "RegFreeCOM\x64\Release\WBFL*.lib"	"%BINTARGET%\Lib\x64\"
-xcopy /y /d  "RegFreeCOM\Win32\Release\WBFL*.dll"	"%BINTARGET%\Libraries\Win32\"
-xcopy /y /d  "RegFreeCOM\Win32\Release\WBFL*.lib"	"%BINTARGET%\Lib\Win32\"
+
 
 xcopy /y /d  License.rtf 			"%BINTARGET%"
 
-REM - Help files and documentation
-
-xcopy /y /d  RegFreeCOM\*.chm			"%BINTARGET%\App\"
+REM - Other important files
 xcopy /y /d  License.txt			"%BINTARGET%\App\"
