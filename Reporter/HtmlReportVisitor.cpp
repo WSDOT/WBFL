f237195c2cbdcf49164d20ab01e5e4ed816ca415
boutilr <115045413+boutilr@users.noreply.github.com>
2023-10-25 14:16:45 -0700

    fixes CSS style sheet (ignore last)

--- Reporter/HtmlReportVisitor.cpp
+++ Reporter/HtmlReportVisitor.cpp
@@ -50,15 +50,23 @@ void rptHtmlReportVisitor::VisitReport(rptReport* pReport)
 
    // If heading numbers are enabled, include CSS for heading numbers in the generated HTML
    if (pReport->HeadingNumbersEnabled()) {
-       *m_pOstream << _T("<style>") << std::endl;
-       *m_pOstream << _T("   h1:before {counter-increment: h1; content: counter(h1) '. ';}") << std::endl;
-       *m_pOstream << _T("   h2:before {counter-increment: h2; content: counter(h2) '. ';}") << std::endl;
-       *m_pOstream << _T("   h3:before {counter-increment: h3; content: counter(h3) '. ';}") << std::endl;
-       *m_pOstream << _T("   h4:before {counter-increment: h4; content: counter(h4) '. ';}") << std::endl;
-       *m_pOstream << _T("   h5:before {counter-increment: h5; content: counter(h5) '. ';}") << std::endl;
-       *m_pOstream << _T("   h6:before {counter-increment: h6; content: counter(h6) '. ';}") << std::endl;
-       *m_pOstream << _T("</style>") << std::endl;
-   }
+        *m_pOstream << _T("<style>") << std::endl;
+        *m_pOstream << _T("body {counter-reset: h1}") << std::endl;
+        *m_pOstream << _T("h1 {counter-reset: h2}") << std::endl;
+        *m_pOstream << _T("h2 {counter-reset: h3}") << std::endl;
+        *m_pOstream << _T("h3 {counter-reset: h4}") << std::endl;
+        *m_pOstream << _T("h4 {counter-reset: h5}") << std::endl;
+        *m_pOstream << _T("h5 {counter-reset: h6}") << std::endl;
+        *m_pOstream << _T("h1:before {counter-increment: h1; content: counter(h1) '. '}") << std::endl;
+        *m_pOstream << _T("h2:before {counter-increment: h2; content: counter(h1) '.' counter(h2) '. '}") << std::endl;
+        *m_pOstream << _T("h3:before {counter-increment: h3; content: counter(h1) '.' counter(h2) '.' counter(h3) '. '}") << std::endl;
+        *m_pOstream << _T("h4:before {counter-increment: h4; content: counter(h1) '.' counter(h2) '.' counter(h3) '.' counter(h4) '. '}") << std::endl;
+        *m_pOstream << _T("h5:before {counter-increment: h5; content: counter(h1) '.' counter(h2) '.' counter(h3) '.' counter(h4) '.' counter(h5) '. '}") << std::endl;
+        *m_pOstream << _T("h6:before {counter-increment: h6; content: counter(h1) '.' counter(h2) '.' counter(h3) '.' counter(h4) '.' counter(h5) '.' counter(h6) '. '}") << std::endl;
+        *m_pOstream << _T("h1.nocount:before, h2.nocount:before, h3.nocount:before, h4.nocount:before, h5.nocount:before, h6.nocount:before { content: ''; counter-increment: none; }") << std::endl;
+        *m_pOstream << _T("</style>") << std::endl;
+    }
+
 
    // Use html helper to Write out style block
    m_Helper.VisitFontLibrary(*m_pOstream);
