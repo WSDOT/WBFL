911bd2343e6720c045d556358165d75c5be5aceb
boutilr <115045413+boutilr@users.noreply.github.com>
2023-11-07 10:42:59 -0800

    pulling dummy Reporter test

---
+++ Reporter/ReporterUnitTests/ReporterUnitTests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ReporterUnitTests
+{
+	TEST_CLASS(ReporterUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::IsTrue(false);
+		}
+	};
+}
