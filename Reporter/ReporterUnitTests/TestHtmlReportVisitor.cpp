cebe23ba261afb66fb8190deea84191c9e4b12be
boutilr <115045413+boutilr@users.noreply.github.com>
2023-11-09 14:55:18 -0800

    Started a chapter builder test
    Now we can start building a bogus chapter builder and test all kinds of report content.

--- Reporter/ReporterUnitTests/TestHtmlReportVisitor.cpp
+++ Reporter/ReporterUnitTests/TestHtmlReportVisitor.cpp
@@ -4,39 +4,66 @@
 #include <Reporter\HtmlReportVisitor.h>
 
 
+#include <fstream>
+#include <sstream>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace ReporterUnitTests
 {
-	TEST_CLASS(HtmlReportVisitorUnitTests)
-	{
-	public:
+    TEST_CLASS(HtmlReportVisitorUnitTests)
+    {
+    public:
 
-		TEST_METHOD(VisitReportTest)
-		{
+        TEST_METHOD(VisitReportTest)
+        {
+            // Creating a test report with a chapter and paragraph
+            rptChapter* pChapter = new rptChapter;
+            rptParagraph* pPara = new rptParagraph;
+            (*pChapter) << pPara;
+            (*testReport) << pChapter;
 
-			std::wstring m_Filename = L"output.html";
+            // Adding some text to the paragraph
+            *pPara << _T("This is a test");
 
-			std::_tofstream ofile(m_Filename.c_str());
+            // Defining the output filename
+            std::wstring m_Filename = L"output.html";
 
-			rptHtmlReportVisitor visitor(&ofile, m_LogPixelsX, m_LogPixelsY);
+            // Creating an output file stream
+            std::_tofstream ofile(m_Filename.c_str());
 
-			visitor.VisitReport(testReport.get());
+            // Initializing the report visitor
+            rptHtmlReportVisitor visitor(&ofile, m_LogPixelsX, m_LogPixelsY);
 
-			ofile.close(); // Close the file after writing to it
+            // Visiting the report
+            visitor.VisitReport(testReport.get());
 
-			std::ifstream file(m_Filename); // Open the file for reading
-			std::string fileContents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>()); // Read the entire file into a string
+            // Closing the output file
+            ofile.close();
 
-			Assert::IsTrue(fileContents.find("HTML") != std::string::npos); // Perform the necessary checks on the file content
+            // Opening the file to read its contents
+            std::ifstream file(m_Filename);
+            std::string fileContents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
+            // Performing checks on the file content
+            Assert::IsTrue(fileContents.find("<!DOCTYPE HTML PUBLIC \"-//Spyglass//DTD HTML 2.0 Extended//EN\">") != std::string::npos);
+            Assert::IsTrue(fileContents.find("<html>") != std::string::npos);
+            Assert::IsTrue(fileContents.find("<head>") != std::string::npos);
+            Assert::IsTrue(fileContents.find("<meta title=\"WSDOT Engineering Reporter on") != std::string::npos);
+//            Assert::IsTrue(fileContents.find("<title>\"WSDOT Engineering Reporter on") != std::string::npos);
+            Assert::IsTrue(fileContents.find("<BODY onDragStart=\"onde()\" >") != std::string::npos);
+            Assert::IsTrue(fileContents.find("<SCRIPT LANGUAGE=\"JavaScript\">") != std::string::npos);
+            Assert::IsTrue(fileContents.find("</html>") != std::string::npos);
 
-		}
+            // Also, check for the presence of the specific text
+            Assert::IsTrue(fileContents.find("This is a test") != std::string::npos);
+        }
 
-	private:
+    private:
 
-		std::shared_ptr<rptReport> testReport = std::make_shared<rptReport>(_T("Test Report"));
-		Uint32 m_LogPixelsX{ static_cast<Uint32>(96) };
-		Uint32 m_LogPixelsY{ static_cast<Uint32>(96) };
-	};
+        std::shared_ptr<rptReport> testReport = std::make_shared<rptReport>(_T("Test Report"));
+        Uint32 m_LogPixelsX{ static_cast<Uint32>(96) };
+        Uint32 m_LogPixelsY{ static_cast<Uint32>(96) };
+    };
 }
+
